24ce5e1c9fecf43d89ece59f9c607b319045b9d3
Frank Robenalt <frank.robenalt@gmail.com>
2018-04-07 17:55:32 -0400

    brollebbects

--- index.js
+++ index.js
@@ -9,32 +9,32 @@ const bg = document.getElementById('about').getBoundingClientRect();
 // document.getElementById('bgtwo').style.height = bgEnd.bottom - bgtwo.y + 300 + 'px';
 
 // function addBackgrounds() {
-//     window.scrollTo(0, 0);
-//     const bg = document.getElementById('about').getBoundingClientRect();
-//     const bgtwo = document.getElementById('projects').getBoundingClientRect();
-//     const bgEnd = document.getElementById('contact').getBoundingClientRect();
-//     document.getElementById('bgone').style.backgroundImage = "url('./images/bg.svg')";
-//     document.getElementById('bgtwo').style.backgroundImage = "url('./images/feather-bg.svg')";
-//     document.getElementById('bgtwo').style.top = bg.bottom - 300 + 'px';
-//     document.getElementById('bgtwo').style.height = bgEnd.bottom - bgtwo.y + 'px';
-// }
-
-
-// setTimeout(function () {
-//     const icon = document.getElementById('one');
-//     icon.style.marginLeft = '0%';
-// }, 500);
-
-// setTimeout(function () {
-//     let icon = document.getElementById('two');
-//     icon.style.marginLeft = '0%';
-// }, 1000);
-
-// setTimeout(function () {
-//     let icon = document.getElementById('three');
-//     icon.style.marginLeft = '0%';
-// }, 1500);
-
+    //     window.scrollTo(0, 0);
+    //     const bg = document.getElementById('about').getBoundingClientRect();
+    //     const bgtwo = document.getElementById('projects').getBoundingClientRect();
+    //     const bgEnd = document.getElementById('contact').getBoundingClientRect();
+    //     document.getElementById('bgone').style.backgroundImage = "url('./images/bg.svg')";
+    //     document.getElementById('bgtwo').style.backgroundImage = "url('./images/feather-bg.svg')";
+    //     document.getElementById('bgtwo').style.top = bg.bottom - 300 + 'px';
+    //     document.getElementById('bgtwo').style.height = bgEnd.bottom - bgtwo.y + 'px';
+    // }
+    
+    
+    // setTimeout(function () {
+        //     const icon = document.getElementById('one');
+        //     icon.style.marginLeft = '0%';
+        // }, 500);
+        
+        // setTimeout(function () {
+            //     let icon = document.getElementById('two');
+            //     icon.style.marginLeft = '0%';
+            // }, 1000);
+            
+            // setTimeout(function () {
+                //     let icon = document.getElementById('three');
+                //     icon.style.marginLeft = '0%';
+                // }, 1500);
+                
 const iconParent = document.getElementById('icons');
 const icons = iconParent.querySelectorAll('.icon-box');
 
@@ -43,6 +43,9 @@ for (let i=0; i<icons.length; i++){
         icons[i].classList.add('intro');
     }, 300);
 }
+
+document.documentElement.style.overflow = 'hidden';
+document.body.scroll = "no";
 
 setTimeout(function() {
     
@@ -53,12 +56,15 @@ setTimeout(function() {
         document.getElementById('loading').style.transform = 'translateY(-110%)';
     }, 1300);
     setTimeout(function() {
+        document.documentElement.style.overflow = 'auto'; 
+        document.documentElement.style.overflowX = 'hidden'; 
+        document.body.scroll = "yes";
         document.getElementsByClassName('name-wrapper')[0].classList.add('intro');
         document.getElementById('icons').classList.remove('before');
         document.getElementById('icons').classList.add('intro');
         clearInterval(dotFunc)
     }, 1600);
-   
+    
 }, 3000);
 
 const dotFunc = setInterval(addDot, 250);
@@ -80,12 +86,6 @@ function removeDots(){
         if (idx === 0){ return }
         cur.style.opacity = '0';
     })
-    // if(dotCount === 0){
-    //     dotCount = 1;
-    //     return addDot();
-    // }
-    // dots[dotCount].style.opacity = '0';
-    // dotCount--;
 }
 
 const airbnb = ["https://d2ffutrenqvap3.cloudfront.net/items/0M063z0P2G2c3o3z3A0O/Screen%20recording%202018-02-18%20at%2004.46.14%20PM.gif", "https://d2ffutrenqvap3.cloudfront.net/items/2x0n11183C3G2x2E2w1W/Screen%20recording%202018-02-18%20at%2004.49.40%20PM.gif", "https://d2ffutrenqvap3.cloudfront.net/items/1i0v2v313b2U1u1p1K2n/Screen%20recording%202018-02-18%20at%2004.50.57%20PM.gif", "https://d2ffutrenqvap3.cloudfront.net/items/1i0v2v313b2U1u1p1K2n/Screen%20recording%202018-02-18%20at%2004.50.57%20PM.gif"];
@@ -163,7 +163,9 @@ function scrollContact() {
     contact.scrollIntoView({ behavior: "smooth", inline: "start" });
 }
 
+
 window.addEventListener('scroll', scroll);
+window.addEventListener('scroll', scrollActions);
 
 function scroll(event){
     if (window.scrollY > bg.top - 200) {
@@ -173,6 +175,30 @@ function scroll(event){
     else if (window.scrollY <= bg.top - 200) {
         document.getElementById('scrollNav').style.padding = '10px'; 
         document.getElementById('navName').style.opacity = '0';
-    } 
+    }
 }
 
+const gifContainers = body.querySelectorAll('.pics-container')
+let gifContainerTops = [];
+gifContainers.forEach((cur, idx)=>{
+    console.log(cur.getBoundingClientRect())
+    gifContainerTops.push(cur.getBoundingClientRect().top);
+})
+const height = window.innerHeight;
+console.log(gifContainerTops);
+
+function scrollActions(event){
+    if (window.scrollY > gifContainerTops[3] - (height/(1.33))){
+                gifContainers[3].style.transform = 'translateY(0px)';
+                gifContainers[3].style.opacity = '1';
+    } else if (window.scrollY > gifContainerTops[2] - (height/(1.33))){
+        gifContainers[2].style.transform = 'translateY(0px)';
+        gifContainers[2].style.opacity = '1';
+    } else if (window.scrollY > gifContainerTops[1] - (height/(1.33))){
+        gifContainers[1].style.transform = 'translateY(0px)';
+        gifContainers[1].style.opacity = '1';
+    } else if (window.scrollY > gifContainerTops[0] - (height/(1.33))){
+        gifContainers[0].style.transform = 'translateY(0px)';
+        gifContainers[0].style.opacity = '1';
+    }
+}
